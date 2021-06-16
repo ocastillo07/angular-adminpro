e2b1a9994db41cb09cf8f8d43080b4f4363ecf99
Oscar <oscarcastillog7@gmail.com>
2021-06-16 18:07:21 -0500

    Fix: API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'https://www.ogcas.tech/api/v1',
+  base_url: 'https://www.ogcas.tech/api',
   client_id: 'lB9eX7riyHgyxpildFRnsMZ0eJBadMdwjjmSSsVIknA',
   client_secret: '9fRRQQsMoMTYmphIgZzGTTmq2xspki4Saq1TkTPoK-g',
 };
